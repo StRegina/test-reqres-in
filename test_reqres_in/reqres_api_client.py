1eeb3cc50480da3a8278f3b44cb877719d53cce2
Regina <st.reginaaa@gmail.com>
2020-09-08 21:10:04 +0500

    Добавление метода request, который принимает: метод реквеста, path, data

--- test_reqres_in/reqres_api_client.py
+++ test_reqres_in/reqres_api_client.py
@@ -1,72 +1,82 @@
-# Добавляем все в консоль, потом добавляем ReqresApiClient.get_delayed_response() и получаем ответ и так со всеми функциями
+# Добавляем в консоль по очереди
+# from test_reqres_in.reqres_api_client import ReqresApiClient
+# client=ReqresApiClient()
+# resp=client.get_delayed_response()
+
 import requests
 class ReqresApiClient:
 
-    base_path = 'https://reqres.in/api/'
+    def __init__(self):
+        self.base_path = 'https://reqres.in/api/'
 
-    def get_delayed_response():
-            response = requests.get(f'{base_path}users?delay=3')
-            return response
+    def request(self, method:str, path:str, data=None):
+        url = self.base_path + path
+        response = requests.request(method, url)
+        return response
+
+    def get_delayed_response(self):
+        response = self.request('GET', 'users?delay=3')
+        return response
 
     def get_list_resourse():
-            response = requests.get(base_path+'unknown')
-            return response
+        response = requests.get(base_path+'unknown')
+        return response
 
     def get_list_users():
-            response = requests.get('https://reqres.in/api/users?page=2')
-            return response
+        response = requests.get('https://reqres.in/api/users?page=2')
+        return response
 
     def get_single_resourse():
-            response = requests.get('https://reqres.in/api/unknown/2')
-            return response
+        response = requests.get('https://reqres.in/api/unknown/2')
+        return response
 
     def get_single_resourse_not_found():
-            response = requests.get('https://reqres.in/api/unknown/23')
-            return response
+        response = requests.get('https://reqres.in/api/unknown/23')
+        return response
 
     def get_single_users():
-            response = requests.get('https://reqres.in/api/users/2')
-            return response
+        response = requests.get('https://reqres.in/api/users/2')
+        return response
 
     def get_single_users_not_found():
-            response = requests.get('https://reqres.in/api/users/23')
-            return response
+        response = requests.get('https://reqres.in/api/users/23')
+        return response
 
     def delete_user():
-            response = requests.delete('https://reqres.in/api/users/2')
-            return response
+        response = requests.delete('https://reqres.in/api/users/2')
+        return response
 
     def post_create():
-            pload = {"name": "morpheus", "job": "leader"}
-            response = requests.post('https://reqres.in/api/users', data=pload)
-            return response
+        pload = {"name": "morpheus", "job": "leader"}
+        response = requests.post('https://reqres.in/api/users', data=pload)
+        return response
 
     def post_create():
-            pload = {"name": "morpheus", "job": "leader"}
-            response = requests.post('https://reqres.in/api/users', data=pload)
-            return response
+        pload = {"name": "morpheus", "job": "leader"}
+        response = requests.post('https://reqres.in/api/users', data=pload)
+        return response
 
     def post_login_successful():
-            pload = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
-            response = requests.post('https://reqres.in/api/login', data=pload)
-            return response
+        pload = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
+        response = requests.post('https://reqres.in/api/login', data=pload)
+        return response
 
     def post_login_unsuccessful():
-            pload = {"email": "peter@klaven"}
-            response = requests.post('https://reqres.in/api/login', data=pload)
-            return response
+        pload = {"email": "peter@klaven"}
+        response = requests.post('https://reqres.in/api/login', data=pload)
+        return response
 
     def post_register_successful():
-            pload = {"email": "eve.holt@reqres.in", "password": "pistol"}
-            response = requests.post('https://reqres.in/api/register', data=pload)
-            return response
+        pload = {"email": "eve.holt@reqres.in", "password": "pistol"}
+        response = requests.post('https://reqres.in/api/register', data=pload)
+        return response
 
     def post_register_unsuccessful():
-            pload = {"email": "sydney@fife"}
-            response = requests.post('https://reqres.in/api/register', data=pload)
-            return response
+        pload = {"email": "sydney@fife"}
+        response = requests.post('https://reqres.in/api/register', data=pload)
+        return response
 
     def put_create():
-            pload = {"name": "morpheus", "job": "zion resident"}
-            response = requests.put('https://reqres.in/api/users/2', data=pload)
-            return response
+        pload = {"name": "morpheus", "job": "zion resident"}
+        response = requests.put('https://reqres.in/api/users/2', data=pload)
+        return response
