b4cb70483d0d3461106ad1b3411d739d2630afa6
Regina <st.reginaaa@gmail.com>
2020-09-09 18:22:28 +0500

    Более сложные запросы request с Get,post,put,delete,patch

--- test_reqres_in/reqres_api_client.py
+++ test_reqres_in/reqres_api_client.py
@@ -1,7 +1,7 @@
 # Добавляем в консоль по очереди
-# from test_reqres_in.reqres_api_client import ReqresApiClient
-# client=ReqresApiClient()
-# resp=client.get_delayed_response()
+# from test_reqres_in.reqres_api_client import ReqresApiClient - папка в котором хранится документ и сам документ
+# client=ReqresApiClient() - создаем клиента
+# resp=client.get_delayed_response() - у каждой функции свой метод
 
 import requests
 class ReqresApiClient:
@@ -18,65 +18,65 @@ class ReqresApiClient:
         response = self.request('GET', 'users?delay=3')
         return response
 
-    def get_list_resourse():
-        response = requests.get(base_path+'unknown')
+    def get_list_resourse(self):
+        response = self.request('GET', 'unknown')
         return response
 
-    def get_list_users():
-        response = requests.get('https://reqres.in/api/users?page=2')
+    def get_list_users(self):
+        response = self.request('GET', 'users?page=2')
         return response
 
-    def get_single_resourse():
-        response = requests.get('https://reqres.in/api/unknown/2')
+    def get_single_resourse(self):
+        response = self.request('GET', 'unknown/2')
         return response
 
-    def get_single_resourse_not_found():
-        response = requests.get('https://reqres.in/api/unknown/23')
+    def get_single_resourse_not_found(self):
+        response = self.request('GET', 'unknown/23')
         return response
 
-    def get_single_users():
-        response = requests.get('https://reqres.in/api/users/2')
+    def get_single_users(self):
+        response = self.request('GET', 'users/2')
         return response
 
-    def get_single_users_not_found():
-        response = requests.get('https://reqres.in/api/users/23')
+    def get_single_users_not_found(self):
+        response = self.request('GET', 'users/23')
         return response
 
-    def delete_user():
-        response = requests.delete('https://reqres.in/api/users/2')
+    def delete_user(self):
+        response = self.request('DELETE', 'users/2')
         return response
 
-    def post_create():
+    def post_create(self):
         pload = {"name": "morpheus", "job": "leader"}
-        response = requests.post('https://reqres.in/api/users', data=pload)
+        response = self.request('POST','users', data=pload)
         return response
 
-    def post_create():
-        pload = {"name": "morpheus", "job": "leader"}
-        response = requests.post('https://reqres.in/api/users', data=pload)
-        return response
-
-    def post_login_successful():
+    def post_login_successful(self):
         pload = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
-        response = requests.post('https://reqres.in/api/login', data=pload)
+        response = self.request('POST','login', data=pload)
         return response
 
-    def post_login_unsuccessful():
+    def post_login_unsuccessful(self):
         pload = {"email": "peter@klaven"}
-        response = requests.post('https://reqres.in/api/login', data=pload)
+        response = self.request('POST','login', data=pload)
         return response
 
-    def post_register_successful():
+    def post_register_successful(self):
         pload = {"email": "eve.holt@reqres.in", "password": "pistol"}
-        response = requests.post('https://reqres.in/api/register', data=pload)
+        response = self.request('POST','register', data=pload)
         return response
 
-    def post_register_unsuccessful():
+    def post_register_unsuccessful(self):
         pload = {"email": "sydney@fife"}
-        response = requests.post('https://reqres.in/api/register', data=pload)
+        response = self.request('POST','register', data=pload)
         return response
 
-    def put_create():
+    def put_create(self):
         pload = {"name": "morpheus", "job": "zion resident"}
-        response = requests.put('https://reqres.in/api/users/2', data=pload)
+        response = self.request('PUT','users/2', data=pload)
+        return response
+
+    def test_patch_update(self):
+        pload = {"name": "morpheus", "job": "zion resident"}
+        response = self.request('PATCH','users/2', data=pload)
         return response
