a2b7a51273d8e9d93a082e18ff9db193c45b95b8
Regina <st.reginaaa@gmail.com>
2020-09-16 22:13:09 +0500

    Изменение типа data со str на dict, т.к. это словарь

--- api_client.py
+++ api_client.py
@@ -1,7 +1,8 @@
 # Добавляем в консоль по очереди
 # from api_client import ReqresApiClient - папка в котором хранится документ и сам документ
 # client=ReqresApiClient() - создаем клиента
-# resp=client.get_delayed_response(тут передеть значения) - у каждой функции свой метод
+# resp=client.get_delayed_response(тут передеть значения через запятую) - у каждой функции свой метод
+
 import requests
 class ReqresApiClient():
 
@@ -45,37 +46,37 @@ class ReqresApiClient():
         response = self.request('DELETE', 'users/{user_id}')
         return response
 
-    def post_create(self, data: str):
+    def post_create(self, data: dict):
         #pload = {"name": "morpheus", "job": "leader"}
         response = self.request('POST','users', data)
         return response
 
-    def post_login_successful(self, data: str):
+    def post_login_successful(self, data: dict):
         #pload = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
         response = self.request('POST','login', data)
         return response
 
-    def post_login_unsuccessful(self, data: str):
+    def post_login_unsuccessful(self, data: dict):
         #pload = {"email": "peter@klaven"}
         response = self.request('POST','login', data)
         return response
 
-    def post_register_successful(self, data: str):
+    def post_register_successful(self, data: dict):
         #pload = {"email": "eve.holt@reqres.in", "password": "pistol"}
         response = self.request('POST','register', data)
         return response
 
-    def post_register_unsuccessful(self, data: str):
+    def post_register_unsuccessful(self, data: dict):
         #pload = {"email": "sydney@fife"}
         response = self.request('POST','register', data)
         return response
 
-    def put_create(self, user_id: int, data: str):
+    def put_create(self, user_id: int, data: dict):
         #pload = {"name": "morpheus", "job": "zion resident"}
         response = self.request('PUT','users/{user_id}', data)
         return response
 
-    def patch_update(self, user_id: int, data: str):
+    def patch_update(self, user_id: int, data: dict):
         #pload = {"name": "morpheus", "job": "zion resident"}
         response = self.request('PATCH','users/{user_id}', data)
         return response
